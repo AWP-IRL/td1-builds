e98527dfaf92b5d364c23496b2654ba00423b1f6
AWP-IRL <awp_irl@proton.me>
2026-05-08 19:09:56 -0400

    Update menu.js

--- docs/assets/js/menu.js
+++ docs/assets/js/menu.js
@@ -1,7 +1,6 @@
 function loadHTML() {
   fetch("./././menu.html")
       .then(response => response.text())
-      .then(text => document.getElementById("menu-nav").innerHTML = text);
-      
+      .then(text => document.getElementById("menu-nav").innerHTML = text);      
 }
 window.onload = loadHTML();
